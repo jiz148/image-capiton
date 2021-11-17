48f3b028bd8f9126fe9d16b9f4b7e9a027e6abae
jiz148 <jizjiz148148@gmail.com>
2021-11-17 18:22:32 -0500

    created project

---
+++ data_prep/data_preparation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def data_prep():
+    df = pd.read_csv('data/wikipedia-image-caption/train-00000-of-00005.tsv',
+                     encoding='utf8',
+                     delimiter='\t',
+                     nrows=10000000
+                     )
+    # filter english
+    df = df[df['language'] == 'en']
+
+    # filter columns
+    df = df.filter(items=['image_url',
+                          'caption_reference_description',
+                          'caption_attribution_description',
+                          'caption_alt_text_description']
+                   )
+
+    df.to_csv('data/preprocessed_data.tsv', sep='\t', index=False)
+    return df.describe()
+
+
+print(data_prep())
---
+++ data_prep/data_processor.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import requests
+from os import cpu_count
+
+import datatable as dt
+import asks
+import trio
+import pandas as pd
+
+
+class PicDownloader:
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.i = 0
+
+        Path('./data_images').mkdir(exist_ok=True)
+
+        img_urls = dt.fread(data_path, sep='\t', columns={'image_url'})
+        # self.links = img_urls.to_list()[0][:10000]
+        self.links = img_urls.to_list()[0][:]
+        print(len(self.links))
+
+    @staticmethod
+    async def fetch_pic(s, url):
+        r = await s.get(url)
+        return r.content
+
+    async def save_pic(self, s, url):
+        content = await self.fetch_pic(s, url)
+        filename = f"data_images/image" + str(self.i) + ".png"
+        with open(filename, 'wb') as f:
+            f.write(content)
+            self.i += 1
+
+    async def main(self):
+        dname = 'https://upload.wikimedia.org'
+        s = asks.sessions.Session(dname, connections=cpu_count() * 2)
+        async with trio.open_nursery() as n:
+            for url in self.links:
+                n.start_soon(self.save_pic, s, url)
+
+
+if __name__ == "__main__":
+    downloader = PicDownloader('data/preprocessed_data.tsv')
+
+    trio.run(downloader.main)
